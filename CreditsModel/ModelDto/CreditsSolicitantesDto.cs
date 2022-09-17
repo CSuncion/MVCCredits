e9118891444f5d5a08ad76bc97f80b77148f8977
Cristhian Suncion <csuncion91@outlook.com>
2022-09-17 01:34:59 -0500

    model solicitantes

---
+++ CreditsModel/ModelDto/CreditsSolicitantesDto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditsModel.ModelDto
+{
+    public class CreditsSolicitantesDto
+    {
+
+        public string Id_Solicitante { get; set; }
+        public string Dni_Solic { get; set; }
+        public string Codofin_Solic { get; set; }
+        public string Carnet_Solic { get; set; }
+        public string Paterno { get; set; }
+        public string Materno { get; set; }
+        public string Nombres { get; set; }
+        public string Sexo { get; set; }
+        public string Situac { get; set; }
+        public string Und_dscto { get; set; }
+        public string Grado { get; set; }
+        public string Fnace { get; set; }
+        public string Domicilio { get; set; }
+        public string Dpto { get; set; }
+        public string Prov { get; set; }
+        public string Dist { get; set; }
+        public string Fijo { get; set; }
+        public string Movil { get; set; }
+        public string Mail { get; set; }
+        public string NroBen { get; set; }
+        public string IdBca { get; set; }
+        public string NumCta { get; set; }
+        public string CCI { get; set; }
+
+    }
+}
